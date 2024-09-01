97e727e7fa176c49baaacdb49ddfd4d37f581858
Shreya chincholkar <chincholkarshreya@gmail.com>
2024-09-01 20:11:33 +0530

    Revert "Few changes done"
    This reverts commit 7a1a15ff7490ec29829536a9226cba99096b315e.

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import '@fortawesome/fontawesome-svg-core';
 import { config } from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss =  false;
 
-import { Inter, Albert_Sans, Playfair_Display_SC, Antonio, Alexandria, Raleway, Anek_Tamil, Libre_Bodoni, Red_Rose, Tourney, Monoton, Anton, Big_Shoulders_Display, Cabin, Poppins, Outfit} from "next/font/google";
+import { Inter, Albert_Sans, Bodoni_Moda, Antonio, Alexandria, Bricolage_Grotesque, Anek_Tamil, Libre_Bodoni, Red_Rose, Tourney, Monoton, Anton, Big_Shoulders_Display, Cabin, Nunito_Sans, Outfit} from "next/font/google";
 import "./globals.css";
 
 const albert_Sans = Albert_Sans({
@@ -11,10 +11,10 @@ const albert_Sans = Albert_Sans({
   variable: "--font-albert_Sans"
 })
 
-const playfair_Display_SC = Playfair_Display_SC({
+const bodoni_Moda = Bodoni_Moda({
   subsets: ['latin'],
   weight: "400",
-  variable: "--font-playfair_Display_SC"
+  variable: "--font-bodoni_Moda"
 })
 
 const antonio = Antonio({
@@ -29,10 +29,10 @@ const alexandria = Alexandria({
   variable: "--font-alexandria"
 })
 
-const raleway = Raleway({
+const bricolage_Grotesque = Bricolage_Grotesque({
   subsets: ['latin'],
   weight: "700",
-  variable: "--font-raleway"
+  variable: "--font-bricolage_Grotesque"
 })
 
 const anek_Tamil = Anek_Tamil({
@@ -83,10 +83,10 @@ const cabin = Cabin({
   variable: "--font-cabin"
 })
 
-const poppins = Poppins({
+const nunito_Sans = Nunito_Sans({
   subsets: ['latin'],
   weight: ['500', '800'],
-  variable: "--font-poppins_Sans"
+  variable: "--font-nunito_Sans"
 })
 
 const outfit = Outfit({
@@ -95,13 +95,21 @@ const outfit = Outfit({
   variable: "--font-outfit"
 })
 
+
+
+
+
+
+
+
+
 const inter = Inter({ subsets: ["latin"] });
 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} ${albert_Sans.variable} ${playfair_Display_SC.variable} ${antonio.variable} ${alexandria.variable} ${raleway.variable} ${anek_Tamil.variable}  ${libre_Bodoni.variable} ${red_Rose.variable} ${tourney.variable} ${monoton.variable} ${anton.variable} ${big_Shoulders_Display.variable} ${cabin.variable} ${outfit.variable} ${poppins.variable}`}>{children}</body>
+      <body className={`${inter.className} ${albert_Sans.variable} ${bodoni_Moda.variable} ${antonio.variable} ${alexandria.variable} ${bricolage_Grotesque.variable} ${anek_Tamil.variable}  ${libre_Bodoni.variable} ${red_Rose.variable} ${tourney.variable} ${monoton.variable} ${anton.variable} ${big_Shoulders_Display.variable} ${cabin.variable} ${outfit.variable}`}>{children}</body>
     </html>
   );
 }
